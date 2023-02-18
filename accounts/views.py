d240b7f6aefacad232b29ee59461d4c87c2b67bf
Pablo Noori <pablo@noori.com>
2023-02-18 23:50:19 +0330

    Bug fixed

--- accounts/views.py
+++ accounts/views.py
@@ -33,6 +33,7 @@ def ssh_create_view(request):
 def ssh_delete_view(request, pk):
     get_user = models.SSH_accounts.objects.get(pk=pk)
     print(get_user)
+    os.system("killall -u {}".format(get_user))
     os.system("userdel {} -r".format(get_user))
     get_user.delete()
     return redirect('accounts:ssh')
